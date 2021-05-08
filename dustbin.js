92d03c9a4668143add6220f873d552dd2c86e3e2
tejasva255 <68278445+tejasva255@users.noreply.github.com>
2021-05-08 16:14:38 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,23 @@
+class dustbin
+{
+    constructor(x,y,width,height)
+    {
+        var option={
+            isStatic:true
+        }
+        this.dustbinObject=Bodies.rectangle(x,y,width,height,option);
+        World.add(world,this.dustbinObject);
+        this.width=width;
+        this.height=height;
+        this.image=loadImage("dustbin.png")
+        
+    }
+
+    display()
+    {
+        fill("lightgrey");
+        rectMode(CENTER);
+        rect(this.dustbinObject.position.x,this.dustbinObject.position.y,this.width,this.height);
+
+    }
+}
